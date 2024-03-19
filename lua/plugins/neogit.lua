c338280e59eb7d6f6994063ad4ebe2a9de427e21
Cristian Fuentes Salgado <cfuentess2010@gmail.com>
2024-03-19 11:23:02 -0300

    Neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,9 +1,13 @@
 return {
   "NeogitOrg/neogit",
+  event = 'VeryLazy',
   dependencies = {
     "nvim-lua/plenary.nvim",         -- required
     "sindrets/diffview.nvim",        -- optional - Diff integration
     "nvim-telescope/telescope.nvim", -- optional
   },
-  config = true
+  config = function ()
+    require('neogit').setup {}
+    vim.keymap.set({'n'}, '<leader>gs', '<cmd>Neogit<cr>', {desc = "Open Neogit"})
+  end
 }
