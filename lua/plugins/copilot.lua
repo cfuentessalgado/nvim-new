f0ed004118352570146f9494154f9b9ea406a5cf
Cristian Fuentes Salgado <cfuentess2010@gmail.com>
2025-05-28 10:09:37 -0400

    use supermaven instead of copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,12 +1,16 @@
 return {
-    "github/copilot.vim",
-    event = "VimEnter",
-    init = function()
-        vim.g.copilot_no_tab_map = false
-        vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
+    -- "github/copilot.vim",
+    -- event = "VimEnter",
+    -- init = function()
+    --     vim.g.copilot_no_tab_map = false
+    --     vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
+    -- end,
+    --
+    -- keys = {
+    --     { "Tab", "copilot#Accept()", { expr = true, silent = true, mode = "i" } },
+    -- }
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+        require("supermaven-nvim").setup({})
     end,
-
-    keys = {
-        { "Tab", "copilot#Accept()", { expr = true, silent = true, mode = "i" } },
-    }
 }
