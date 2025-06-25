60c0fb1d75199a699a5f51f6b2f8a10c407fe15a
Cristian Fuentes Salgado <cfuentess2010@gmail.com>
2025-06-25 07:35:27 -0400

    fix blade syntax

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,11 +6,21 @@ return { -- Highlight, edit, and navigate code
 
         ---@diagnostic disable-next-line: missing-fields
         require('nvim-treesitter.configs').setup {
-            ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'php', 'go', 'javascript' },
+            ensure_installed = { 'bash', 'blade', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'php', 'go', 'javascript' },
             -- Autoinstall languages that are not installed
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
+        }
+        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+        parser_config.blade = {
+            install_info = {
+                url = "https://github.com/EmranMR/tree-sitter-blade",
+                files = { "src/parser.c" },
+                branch = "main",
+            },
+            filetype = "blade"
         }
 
         -- There are additional nvim-treesitter modules that you can use to interact
