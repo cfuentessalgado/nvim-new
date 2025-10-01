072274c066bcca165732dd4b9f60ab2c3283afc3
Cristian Fuentes Salgado <cfuentess2010@gmail.com>
2025-10-01 15:10:43 -0300

    WIP

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,18 +1,11 @@
 return {
   "zbirenbaum/copilot.lua",
-  requires = {
-    "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
-  },
   cmd = "Copilot",
   event = "InsertEnter",
   config = function()
     require("copilot").setup({
-      suggestion = {
-        auto_trigger = false -- TODO: check if needs to be true
-      },
-      nes = {
-        enabled = true,
-      }
+      suggestion = { enabled = false },
+      panel = { enabled = false },
     })
   end,
 }
