45c6d6d25d87632d06458801ed2b17bdfb9f9311
Cristian Fuentes Salgado <cfuentess2010@gmail.com>
2025-12-13 10:23:17 -0300

    theme and plugins restored

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,9 @@
 return {
   {
     'folke/tokyonight.nvim',
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd("colorscheme catppuccin")
+      vim.cmd("colorscheme tokyonight-night")
     end
   }
 }
